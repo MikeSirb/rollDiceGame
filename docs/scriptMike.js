1fbb6ee695daec206948307167a62928ea96037f
mike <2210859010@fh-burgenland.at>
2023-03-23 14:13:53 +0100

    addet new functions (rules, win, about me)

--- docs/scriptMike.js
+++ docs/scriptMike.js
@@ -1,6 +1,7 @@
 "use strict";
 
 /* === VARIABLEN-SHORTCUTS === */
+const winEl = document.getElementById("limit");
 const score0 = document.getElementById("score--0");
 const score1 = document.getElementById("score--1");
 const currentScore0 = document.getElementById("current--0");
@@ -15,8 +16,17 @@ const holdButton = document.querySelector(".btn--hold");
 const newButton = document.querySelector(".btn--new");
 const dice = document.querySelector(".dice");
 
+const rulesButton = document.querySelector(".btn--rules");
+const aboutButton = document.querySelector(".btn--about");
+const blur = document.querySelector(".blur");
+const close = document.querySelectorAll(".close");
+
+const rulesModal = document.querySelector(".modal--rules");
+const aboutModal = document.querySelector(".modal--about");
+
 /* === VARIABLEN-PROGRAMM === */
-let scores, currentScore, startScore, activePlayer, win;
+
+let scores, currentScore, startScore, activePlayer, win, winPoints;
 let scoreActive = document.querySelector(`#score--${activePlayer}`);
 let playerActiveEl = document.querySelector(`.player--${activePlayer}`);
 let playerEl = document.querySelector(`#name--${activePlayer}`);
@@ -28,21 +38,21 @@ const startCondition = function () {
   currentScore = 0;
   activePlayer = 0;
   win = false;
+  winPoints = winEl.value;
+  winEl.textContent = winPoints;
 
-  document.querySelector("main").style.backgroundColor =
-    "rgb(140, 179, 213, 0.4)";
+  //document.querySelector("main").style.backgroundColor = ("rgb(140, 179, 213, 0.4)");
 
   player0.classList.add("player--active");
-  player1.classList.remove("player--active");
-  player0.style.color = "rgb(45, 59, 96)";
-  player1.style.color = "rgb(45, 59, 96)";
+  player0.classList.remove("winner");
+  player1.classList.remove("player--active", "winner");
+
   namePlayer0.style.color = "rgb(45, 59, 96)";
   namePlayer1.style.color = "rgb(45, 59, 96)";
 
   score0.textContent = currentScore;
   score1.textContent = currentScore;
-  score0.style.color = "rgb(45, 59, 96)";
-  score1.style.color = "rgb(45, 59, 96)";
+
   currentScore0.textContent = currentScore;
   currentScore1.textContent = currentScore;
   dice.classList.add("hidden");
@@ -67,13 +77,29 @@ const switchPlayer = function () {
   playerActiveEl.classList.add("player--active");
 };
 
+// winPlayer
+
 const winPlayer = function (activePlayer) {
   win = true;
   dice.src = `dice-win-${activePlayer}.png`;
   scoreActive.textContent = "Winner";
-  scoreActive.style.color = "#FFF";
   playerEl = document.querySelector(`#name--${activePlayer}`);
-  playerEl.style.color = "#FFF";
+  playerActiveEl.classList.add("winner");
+};
+
+// openModal
+
+const openModal = function (modal) {
+  modal.classList.remove("hidden");
+  blur.classList.remove("hidden");
+};
+
+// closeModal
+
+const closeModal = function () {
+  rulesModal.classList.add("hidden");
+  aboutModal.classList.add("hidden");
+  blur.classList.add("hidden");
 };
 
 /* === ROLL DICE === */
@@ -88,11 +114,12 @@ rollButton.addEventListener("click", function () {
       dice.classList.remove("hidden");
     }, 50); // 50ms
 
+    currentScore += random;
+    scoreActive.textContent = currentScore;
+
     if (random === 1) {
       switchPlayer();
     }
-    currentScore += random;
-    scoreActive.textContent = currentScore;
   }
 });
 
@@ -104,7 +131,7 @@ holdButton.addEventListener("click", function () {
 
     currentScore0.textContent = scores[0];
     currentScore1.textContent = scores[1];
-    if ((scores[0] >= 20) | (scores[1] >= 20)) {
+    if ((scores[0] >= winPoints) | (scores[1] >= winPoints)) {
       winPlayer(activePlayer);
     } else {
       switchPlayer();
@@ -115,3 +142,22 @@ holdButton.addEventListener("click", function () {
 newButton.addEventListener("click", function () {
   startCondition();
 });
+
+/* === MODAL-WINDOW-BUTTONS */
+
+//open
+rulesButton.addEventListener("click", function () {
+  openModal(rulesModal);
+});
+
+aboutButton.addEventListener("click", function () {
+  openModal(aboutModal);
+});
+
+//close
+
+for (let i = 0; i < close.length; i++) {
+  close[i].addEventListener("click", closeModal);
+}
+
+document.addEventListener("keydown", closeModal);
